766d177441da4e4c74b18074f9a5bfb7e90eb8fa
arthurabello <rabelolito@gmail.com>
2025-05-24 15:31:56 -0300

    feature: bst.h written with the first functions, documentation will follow linearly with progress

---
+++ src/bst/bst.h
@@ -0,0 +1,53 @@
+#ifndef BST_H
+#define BST_H
+
+#include <iostream>
+#include <vector>
+
+// Here we will define necessary structures and functions for the binary search tree (BST)
+
+namespace BST {
+    
+    struct Node {
+    std::string word;
+    std::vector<int> documentIds;
+    Node* parent;
+    Node* left;
+    Node* right;
+    int height;    // usado na AVL
+    };
+
+    struct BinaryTree {
+        Node* root;
+    };
+
+    struct InsertResult {
+        int numComparisons;
+        double executionTime;
+    };
+    
+    struct SearchResult {
+        int found;
+        std::vector<int> documentIds;
+        double executionTime;
+        int numComparisons;
+    };
+
+    BinaryTree* create();
+
+        /**
+     * @brief Inserts a word into the binary search tree.
+     * 
+     * Initializes the binary search tree with a nullptr root.
+     */
+
+    Node* CreateNode(std::string word,std::vector<int>documentIds);
+
+    InsertResult Insert(BinaryTree* tree, const std::string& word, int documentId);
+    
+    SearchResult Search(BinaryTree* tree, const std::string& word);
+    
+    void DeleteBinaryTree(BinaryTree* tree);
+}
+
+#endif
