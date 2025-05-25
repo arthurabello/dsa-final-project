430b94e29d66ea4d8282fc13fa74788661778065
gabcarn <gabrielcarneiron@gmail.com>
2025-05-25 19:23:50 -0300

    feat: adding data scrapping archives

--- src/data.h
+++ src/data.h
@@ -2,9 +2,32 @@
 #define DATA_H
 #include <iostream>
 #include <vector>
+#include <fstream>
+#include "tree_utils.h"
 
+namespace DATA {
+    static std::string normalise(std::string w);
+    /**
+     * @brief Converts the string passed as argument
+     * in standard ascii format, i.e all letters in
+     * lowercase and without accents.
+     * 
+     * @param w Word you wish to normalise.buildIndex
+     * @returns Normalised argument
+     */
+    
+    
+    std::vector<std::string> tokenize(std::string filename);
+    /**
+     * 
+     * 
+     * 
+     * 
+     */
 
+    void buildIndex(const std::string& rootFolder, TREE::BinaryTree* tree);
 
-
+    std::vector<TREE::Node*> buildNodes(std::string filename);
+}
 
 #endif
