f336d054d4151d069a42ee34fe48ca1547dad8b6
g4briwlle <gabriellemascarelo@gmail.com>
2025-05-26 17:57:30 -0300

    chore: starting the data reading. stopping cause i dont know which library to use

--- src/data.cpp
+++ src/data.cpp
@@ -1,3 +1,11 @@
 #include <iostream>
 #include <vector>
 #include "data.h"
+
+std::vector<std::string> list_files_txt_in_path(const std::string &dir_path){
+    //
+}
+
+std::string read_file_content(const std::string& full_file_path){
+    //
+}
