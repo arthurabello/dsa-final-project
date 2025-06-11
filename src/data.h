60f1d083b663ff1b586bc1d064bea3110d93b5e3
nmspaniol <nicolasmspaniol@gmail.com>
2025-06-11 00:01:54 -0300

    chore: updates data.h docs and function definitions

--- src/data.h
+++ src/data.h
@@ -6,51 +6,37 @@
 #include "tree_utils.h"
 
 namespace DATA {
+    /**
+     * @brief Makes a string lowercase and removes non-alphanumeric characters.
+     * 
+     * @param string Text you wish to normalise
+     * @returns Normalised string
+     */
     std::string normalise(const std::string& w);
+    
     /**
-     * @brief Converts the string passed as argument
-     * in standard ascii format, i.e all letters in
-     * lowercase and without accents.
-     * 
-     * @param w Word you wish to normalise.buildIndex
-     * @returns Normalised argument
+     * @brief Breaks a text file into a vector of normalized "words".
+     *
+     * This function expects the file to have no punctuation and will only break
+     * the words at spaces.
+     *
+     * @param filename Path to the file with the words you want
+     * @returns A vector with all the words from the file
      */
-    
-    
     std::vector<std::string> tokenize(std::string filename);
+
     /**
-     * 
-     * 
-     * 
-     * 
+     * @brief Retrieves a list of all .txt filenames from a specified directory.
+     *
+     * This function scans the given directory path and returns a vector
+     * containing the names (not full paths) of all files ending with the ".txt"
+     * extension. Returns an empty vector if the directory cannot be accessed or
+     * no .txt files are found.
+     *
+     * @param directory_path The path to the directory to scan
+     * @returns A vector of .txt filenames
      */
-
-    void buildIndex(const std::string& rootFolder, TREE::BinaryTree* tree);
-
-    std::vector<TREE::Node*> buildNodes(std::string filename);
-
     std::vector<std::string> list_txt_files_in_path(const std::string &dir_path);
-
-/**
- * @brief Reads the entire content of a specified file into a string.
- *
- * @param full_file_path The complete path (including filename) to the file to be read.
- * @return std::string The content of the file.
- * Returns an empty string if the file cannot be opened or read.
- */
 }
-
-/**
- * @brief Retrieves a list of all .txt filenames from a specified directory.
- *
- * This function scans the given directory path and returns a vector
- * containing the names (not full paths) of all files ending with the ".txt" extension.
- *
- * @param directory_path The path to the directory to scan.
- * @return std::vector<std::string> A list of .txt filenames.
- * Returns an empty vector if the directory cannot be accessed or no .txt files are found.
- */
-
-std::string read_file_content(const std::string& full_file_path);
 
 #endif
